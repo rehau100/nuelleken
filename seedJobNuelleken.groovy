90173a2188f23f07063415be0332b4e7910e1430
rehau100 <hausmann.rebekka@gmx.de>
2019-09-05 13:51:40 +0200

    First seedjob

---
+++ seedJobNuelleken.groovy
@@ -0,0 +1,25 @@
+import javaposse.jobdsl.Job
+
+def folderName = "Nuelleken"
+folder(folderName)
+
+def myJobName = "Testjob"
+def myJob = job(myJobName)
+
+myJob.with {
+    description("Eine Beschreibung")
+    logRotator(10, 10)
+
+    parameters {
+        stringParam("MAIL_CONTENT", "", "Inhalt der Mail")
+    }
+
+    steps {
+        shell('''echo "Shellausgabe:"
+        echo MAIL_CONTENT: $MAIL_CONTENT''')
+    }
+
+    publishers {
+        mailer('hausmann.rebekka@gmx.com', true, true)
+    }
+}
